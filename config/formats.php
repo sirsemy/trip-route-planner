aee4c4c20b2e5a2f777894cd2f2bb5fac572492c
sirsemy <hajdu1szabolcs@gmail.com>
2023-06-16 12:15:51 +0200

    Init Laravel

---
+++ config/formats.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'date_time_format' => 'Y-m-d H:i:s',
+    'week_day_format' => 'N',
+    'starting_work_hour' => 9,
+    'finishing_work_hour' => 17,
+    'working_hours_per_day' => 8,
+    'number_of_friday' => 5,
+    'number_of_saturday' => 6,
+    'hour_minute_format' => 'H:i',
+    'hour_format' => 'H',
+    'interval_time_start' => 'PT',
+    'interval_three_days' => 'P3D',
+    'interval_one_day' => 'P1D'
+];
